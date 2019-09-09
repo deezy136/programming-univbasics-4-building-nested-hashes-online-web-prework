972ce43154c87ad89197224a15f70bb6c48c213f
deezy13 <deezy13@gmail.com>
2019-09-09 16:15:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -45,7 +45,7 @@ def monopoly_with_third_tier
   	                :key2 => " ",
   	                :key3 => " ",
   	                :key4 => " "
-  	              }
+  	              
   	          }
   	  }
   	}
