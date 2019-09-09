a123d9f2d29d9d0bae2b42756a7b0c68716f2070
deezy13 <deezy13@gmail.com>
2019-09-09 16:04:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,8 +14,8 @@ def monopoly_with_second_tier
   
   	monopoly = {
   	  :railroads => {
-  	                :pieces => 4,
-  	                :rent_in_dollars => {}
+  	                :pieces => 4
+  	               
   	  
   	}}
   
@@ -28,7 +28,10 @@ def monopoly_with_third_tier
   # complexity.
   
   	monopoly = {:railroads => {
-  	  :pieces => 4}}
+  	  :pieces => 4,
+  	    :rent_in_dollars => {}
+  	    
+  	}}
 end
 
 def monopoly_with_fourth_tier
